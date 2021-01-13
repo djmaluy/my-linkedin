926fc93404fb3c131bc5029ca2478ee3ef30ed25
Andrii Tsvirko <a.tsvirko@ukr.net>
2021-01-13 19:47:11 +0200

    initialized project and added Header comp

---
+++ src/components/header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import linkedin from "../../images/linkedin.svg";
+import avatar from "../../images/avatar.jpeg";
+import "./Header.css";
+import HeaderOptions from "../headerOptions/HeaderOptions";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src={linkedin} alt="logo" />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text"></input>
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOptions Icon={HomeIcon} title="Home" />
+        <HeaderOptions Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOptions Icon={BusinessCenterIcon} title="Job" />
+        <HeaderOptions Icon={ChatIcon} title="Messaging" />
+        <HeaderOptions Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOptions avatar={avatar} title="Me" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
